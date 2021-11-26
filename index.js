d85aa06586b9e2b0e42e695a872b9ef5942115ad
Lina Virginia <linavm@unicauca.edu.co>
2021-11-26 11:13:01 -0500

    backend funcional, recibe las solicitudes y las guarda en DB

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ app.use(express.json());
 const routeDispo = require("./router/dispositivos")
 app.use("/dispositivos", routeDispo)
 
+//Llamo a la base de datos y al esquema
+const db = require("./moduls/db");
+const Dispositivo = require('./moduls/dispositivo')
+const mgs = require("mongoose");
+
 //conexión con broker
 var mqtt = require('mqtt')
 var client  = mqtt.connect('mqtt://test.mosquitto.org')
@@ -19,31 +24,37 @@ function EventoConectar() {
 }
 
 function EventoMensaje(topic, message) {
-    var datos = message.toString().split('-');
-    time=datos[0];
-    variable=datos[1];
-    location=datos[2];
-    console.log(time +"*" +variable+"*" +location )
+  var datos = message.toString().split('/');
+  date=datos[0];
+  variable=datos[1];
+  location=datos[2];
+  console.log(date +"*" +variable+"*" +location )
+  var nombre = "Dispositivo no encontrado";
   if(topic=="LAB4Unicauca/Arduino"){
-
-    console.log("Es Arduino")
-  }
-
-  if(topic=="LAB4Unicauca/Esp"){
-
-    console.log("Es Esp")
-  }
+      nombre = "Arduino";
+    }
   
-  if(topic=="LAB4Unicauca/Rasp"){
+    if(topic=="LAB4Unicauca/Esp"){
+      nombre = "Esp";
+    }
+    
+    if(topic=="LAB4Unicauca/Rasp"){
+      nombre = "Rasp";
+    }
+  
+    if(topic=="LAB4Unicauca/Beagle"){
+      nombre = "Beagle";
+    }
 
-    console.log("Es Rasp")
-  }
-
-  if(topic=="LAB4Unicauca/Beagle"){
-
-    console.log("Es Beagle")
-  }
- 
+        //  POST PARA CREAR USUARIO EN MONGO
+      mgs.model("Dispositivo").create({
+          "nombre":  nombre,
+          "variable": variable ,
+          "location": location,
+          "date": date,
+      }, (err,dispositivo)=>{
+          console.log(dispositivo);
+      })
 
   //   client.end()
 }
--- router/dispositivos.js
+++ router/dispositivos.js
@@ -1,34 +1,63 @@
 const express  = require ('express');
 const req = require("express/lib/request");
 const routes = express.Router();
+const app = express();
+app.use(express.json()); 
 
 //Llamo a la base de datos y al esquema
 const db = require("../moduls/db");
 const Dispositivo = require('../moduls/dispositivo')
 const mgs = require("mongoose");
 
-//  POST PARA CREAR USUARIO EN MONGO
-routes.route("/guardar").post((req, resp, next)=> {
-    mgs.model("Dispositivo").create({
-        "nombre":  req.body.nombre,
-        "variable": req.body.variable ,
-        "location": req.body.location,
-        "date": req.body.date,
-    }, (err,dispositivo)=>{
-        console.log(dispositivo);
-        resp.json(dispositivo)
-    })
-})
 
-routes.route("/findByDocument").get((req, resp, next)=> {
-    mgs.model("Dispositivo").find({
-        "nombre":req.query.nombre
-    }, (err,dispositivo)=>{
-        console.log(dispositivo);
-        resp.json(dispositivo)
-    })
+app.use(function (req, res) {
+    var datos = req.mivariablemqtt.toString().split('/');
+    topic=datos[0];
+    date=datos[1];
+    variable=datos[2];
+    location=datos[3];
+    console.log(time +"*" +variable+"*" +location )
+    var nombre = "Dispositivo no encontrado";
+    if(topic=="LAB4Unicauca/Arduino"){
+        nombre = "Arduino";
+      }
     
-})
+      if(topic=="LAB4Unicauca/Esp"){
+        nombre = "Esp";
+      }
+      
+      if(topic=="LAB4Unicauca/Rasp"){
+        nombre = "Rasp";
+      }
+    
+      if(topic=="LAB4Unicauca/Beagle"){
+        nombre = "Beagle";
+      }
+
+          //  POST PARA CREAR USUARIO EN MONGO
+        mgs.model("Dispositivo").create({
+            "nombre":  nombre,
+            "variable": variable ,
+            "location": location,
+            "date": date,
+        }, (err,dispositivo)=>{
+            console.log(dispositivo);
+            resp.json(dispositivo)
+        })
+
+  });
+
+
+
+// routes.route("/findByDocument").get((req, resp, next)=> {
+//     mgs.model("Dispositivo").find({
+//         "nombre":req.query.nombre
+//     }, (err,dispositivo)=>{
+//         console.log(dispositivo);
+//         resp.json(dispositivo)
+//     })
+    
+// })
 
 // router.get('/', (req, res) => {
 //     res.render("dispositivos",{
